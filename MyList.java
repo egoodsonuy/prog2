674693111eb6357361cec4d4187f28fbc0358eed
Sofia Delfino <sdelfino@correo.um.edu.uy>
2022-06-04 11:51:27 -0300

    MyList

---
+++ MyList.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public interface MyList<T> {
+
+    void add(T value);
+
+    T get(int position);
+
+    boolean contains(T value);
+
+    void remove(T value);
+
+    int size();
+}
