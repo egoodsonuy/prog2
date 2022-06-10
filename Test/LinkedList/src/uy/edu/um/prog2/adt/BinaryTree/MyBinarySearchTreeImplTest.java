4bc9138cdfaf430e3a78e371ca6d3a50b4e91607
Sofia <sdelfino@correo.um.edu.uy>
2022-06-10 09:38:21 -0300

    test Binary Tree

---
+++ Test/LinkedList/src/uy/edu/um/prog2/adt/BinaryTree/MyBinarySearchTreeImplTest.java
@@ -0,0 +1,22 @@
+package LinkedList.src.uy.edu.um.prog2.adt.BinaryTree;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyBinarySearchTreeImplTest {
+    @Test
+    public void testPrincipal() {
+        MySearchBinaryTree<Integer, Integer> ArbolP = new MySearchBinaryTreeImpl<>();
+
+        ArbolP.add(3, 478);
+        ArbolP.add(5, 934);
+        ArbolP.add(1, 1000);
+        ArbolP.add(2, 1432);
+
+        assertEquals(null, test.find(4));
+        assertEquals(934, test.find(5));
+
+        test.remove(1);
+        test.add(1, 1111);
+        assertEquals(1111, test.find(1));
+    }
+}
